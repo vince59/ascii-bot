1c13ea10d912fab60a4c9e9d3eadd27cf7aad844
Vincent <vince.pno@gmail.com>
2021-12-05 13:48:13 +0100

    new algo

--- src/robot/robot_main.c
+++ src/robot/robot_main.c
@@ -90,6 +90,9 @@ int test_matrix()
 	return EXIT_SUCCESS;
 }
 
+// Send all map to the mapper
+// map is internal matrix for the robot memory
+
 int update_debug_map()
 {
 	for (int l = 0; l < map.max_l; l++)
@@ -98,6 +101,10 @@ int update_debug_map()
 				return EXIT_FAILURE;
 	return EXIT_SUCCESS;
 }
+
+// enlarge matrix from the robot postion to dist
+// dist is a number of cells
+// North
 
 void update_map_N(int dist)
 {
@@ -111,6 +118,8 @@ void update_map_N(int dist)
 	}
 }
 
+// East
+
 void update_map_E(int dist)
 {
 	int c = map.c + 1 + dist;
@@ -122,6 +131,7 @@ void update_map_E(int dist)
 	}
 }
 
+// South
 void update_map_S(int dist)
 {
 	int l = map.l + 1 + dist;
@@ -133,6 +143,7 @@ void update_map_S(int dist)
 	}
 }
 
+// West
 void update_map_O(int dist)
 {
 	int c = map.c - dist;
@@ -145,6 +156,8 @@ void update_map_O(int dist)
 	}
 }
 
+// Scan area and enlarge matrix if necesary
+// sim_socket : simulator socket
 int update_map(int sim_socket)
 {
 	int dist, info;
@@ -216,6 +229,17 @@ int update_map(int sim_socket)
 	return EXIT_SUCCESS;
 }
 
+// get the shortest path to go from x0, and y0 to x1 and y1
+// dots : array with all the plot for the path
+// dots[0][0] -> column (x) of the first plot
+// dots[0][1] -> row (y) of the first plot
+// dots[1][0] -> column (x) of the second plot
+// dots[1][1] -> row (y) of the second plot
+// dots[n][0] -> column (x) of the n plot
+// dots[n][1] -> row (y) of the n plot
+// returns the number of plot
+// dots have to be allocated before the call -> int dots[10][2] for 10 plots max
+
 int get_path(int x0, int y0, int x1, int y1, int (*dots)[2])
 {
 	int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
@@ -247,7 +271,9 @@ int get_path(int x0, int y0, int x1, int y1, int (*dots)[2])
 	return i - 1;
 }
 
-//jeudi 2 décembre à 14h45
+// find the closest unknown point
+// gives the row number and the colum number of this point
+// returns 0 if all area has been already discovered, 1 otherwhise
 
 int find_uncleared_zone(int *min_l, int *min_c)
 {
@@ -282,6 +308,155 @@ int find_uncleared_zone(int *min_l, int *min_c)
 	return (dis == 9999);
 }
 
+// check if coordonate in not outside the matrix
+
+int out_of_bound_x(int x)
+{
+	if (x < 0)
+		return EXIT_FAILURE;
+	if (x >= map.max_c)
+		return EXIT_FAILURE;
+	return EXIT_SUCCESS;
+}
+
+int out_of_bound_y(int y)
+{
+	if (y < 0)
+		return EXIT_FAILURE;
+	if (y >= map.max_l)
+		return EXIT_FAILURE;
+	return EXIT_SUCCESS;
+}
+
+int out_of_bound(int x, int y)
+{
+	return (!out_of_bound_x(x) && !out_of_bound_y(y));
+}
+
+// Find the closest unknown cell from the bot cell
+// AAAAAAA
+// B     D
+// B  X  D
+// B     D
+// CCCCCCC
+
+int find_unknown_cell(int *x, int *y, int level)
+{
+	int x0 = map.c - level;
+	int y0 = map.l - level;
+	int x1 = map.c + level;
+	int y1 = map.l + level;
+	int length = x1 - x0;
+
+	int stop = 0;
+
+	// A
+	if (!out_of_bound_y(y1))
+	{
+		for (int i = 0; i < length; i++)
+		{
+			int ix0 = x0 + i;
+			if (!out_of_bound_x(ix0) && map.map[ix0][y1].content == UNKNOWN)
+			{
+				*x = ix0;
+				*y = y1;
+				return EXIT_SUCCESS;
+			}
+		}
+	}
+	else
+		stop++;
+
+	// B
+	if (!out_of_bound_x(x0))
+	{
+		for (int i = 0; i < length; i++)
+		{
+			int iy0 = y0 + i;
+			if (!out_of_bound_y(iy0) && map.map[x0][iy0].content == UNKNOWN)
+			{
+				*x = x0;
+				*y = iy0;
+				return EXIT_SUCCESS;
+			}
+		}
+	}
+	else
+		stop++;
+
+	// C
+	if (!out_of_bound_y(y0))
+	{
+		for (int i = 0; i < length; i++)
+		{
+			int ix0 = x0 + i;
+			if (!out_of_bound_x(ix0) && map.map[ix0][y0].content == UNKNOWN)
+			{
+				*x = ix0;
+				*y = y0;
+				return EXIT_SUCCESS;
+			}
+		}
+	}
+	else
+		stop++;
+
+	// D
+	if (!out_of_bound_x(x1))
+	{
+		for (int i = 0; i < length; i++)
+		{
+			int iy0 = y0 + i;
+			if (!out_of_bound_y(iy0) && map.map[x1][iy0].content == UNKNOWN)
+			{
+				*x = x1;
+				*y = iy0;
+				return EXIT_SUCCESS;
+			}
+		}
+	}
+	else
+		stop++;
+
+	if (stop < 4)
+		return find_unknown_cell(x, y, level + 1);
+	return EXIT_FAILURE;
+}
+
+int push_cell(int i, int x, int y, int (*cells)[2])
+{
+	if (!out_of_bound(x, y) && map.map[x][y].content == FREE)
+	{
+		cells[i][0] = x;
+		cells[i][1] = y;
+		i++;
+	}
+	return i;
+}
+
+int find_all_free_adjacent_cells(int x, int y, int (*cells)[2])
+{
+	int i = 0;
+	i = push_cell(i, x - 1, y - 1, cells);
+	i = push_cell(i, x, y - 1, cells);
+	i = push_cell(i, x + 1, y - 1, cells);
+
+	i = push_cell(i, x - 1, y, cells);
+	i = push_cell(i, x + 1, y, cells);
+
+	i = push_cell(i, x - 1, y + 1, cells);
+	i = push_cell(i, x, y + 1, cells);
+	i = push_cell(i, x + 1, y + 1, cells);
+	return i;
+}
+
+// find the direction to go to join a specific point
+// r_l : row of the start point
+// r_c : column of the start point
+// l : row of the destination point
+// c : column of the destination point
+// return the direction N S E O ...
+
 int get_dir_to_cell(int r_l, int r_c, int l, int c)
 {
 	int d_l = r_l - l;
@@ -308,6 +483,8 @@ int get_dir_to_cell(int r_l, int r_c, int l, int c)
 	return SE;
 }
 
+// test program to find a target
+
 int test_find_target(int sim_socket, int mapper_socket)
 {
 	map.max_c = map.max_l = 1;
@@ -320,14 +497,18 @@ int test_find_target(int sim_socket, int mapper_socket)
 	for (;;)
 	{
 		int l, c;
-		if (update_map(sim_socket))
+		if (update_map(sim_socket)) // scan area and update internal matrix
 			return EXIT_FAILURE;
 
-		update_debug_map();
-		
-		if (find_uncleared_zone(&l, &c))
+		update_debug_map(); // display updated map with the mapper
+
+		if (find_unknown_cell(&c, &l, 1)) // find the closest unknown cell from the bot
 			break;
 
+		int cells[8][2];
+		int k=find_all_free_adjacent_cells(map.c, map.l, cells)
+
+		
 		int path[map.max_l * map.max_c][2];
 		int i = get_path(map.c, map.l, c, l, path);
 
@@ -335,7 +516,7 @@ int test_find_target(int sim_socket, int mapper_socket)
 		{
 			int c0 = path[j][0];
 			int l0 = path[j][1];
-			if (map.map[c0][l0].content!=FREE)
+			if (map.map[c0][l0].content != FREE)
 				break;
 
 			int dir = get_dir_to_cell(map.l, map.c, l0, c0);
@@ -345,7 +526,7 @@ int test_find_target(int sim_socket, int mapper_socket)
 			//scanf("%s", s);
 			//if (s[0] == 'q')
 			//	break;
-				delay(50);
+			//	delay(50);
 			if (go_to(dir, &status, sim_socket))
 				return EXIT_FAILURE;
 			if (status != CMD_OK)
@@ -364,9 +545,9 @@ int test_find_target(int sim_socket, int mapper_socket)
 	}
 
 	puts("fini\n");
-				char s[30];
-			scanf("%s", s);
-			/*if (s[0] == 'q')
+	char s[30];
+	scanf("%s", s);
+	/*if (s[0] == 'q')
 				break;*/
 	return EXIT_SUCCESS;
 
